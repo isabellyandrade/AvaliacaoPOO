5b2f1f71fac1207408542b483f01317d40b176fd
isabellyandrade <137850120+isabellyandrade@users.noreply.github.com>
2024-12-04 21:27:07 -0300

    Add files via upload

--- QuebraCabeca/src/quebracabeca/Peca.java
+++ QuebraCabeca/src/quebracabeca/Peca.java
@@ -1,19 +1,36 @@
 
 package quebracabeca;
 
-abstract class Peca {
-    String nome;
+public abstract class Peca {
+    private Borda bordaSuperior;
+    private Borda bordaInferior;
+    private Borda bordaEsquerda;
+    private Borda bordaDireita;
 
-    public abstract boolean encaixarCom(Peca outraPeca);
-
-    public String getNome() {
-        return nome;
+    public Peca(Borda bordaSuperior, Borda bordaInferior, Borda bordaEsquerda, Borda bordaDireita){
+        this.bordaSuperior = bordaSuperior;
+        this.bordaInferior = bordaInferior;
+        this.bordaEsquerda = bordaEsquerda;
+        this.bordaDireita = bordaDireita;
     }
 
-    @Override
-    public String toString() {
-        return "Peca{" + "nome='" + nome + '\'' + '}';
+    public Borda getBordaSuperior() {
+        return bordaSuperior;
     }
+
+    public Borda getBordaInferior() {
+        return bordaInferior;
+    }
+
+    public Borda getBordaEsquerda() {
+        return bordaEsquerda;
+    }
+
+    public Borda getBordaDireita() {
+        return bordaDireita;
+    }
+    
+    public abstract boolean encaixe(Peca outraPeca);
 }
 
 
